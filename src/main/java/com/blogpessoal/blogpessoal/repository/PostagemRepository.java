6bb99cb2d572073ebb053214447aaa98524e8e5e
Felix Augusto <111309593+AugustoFO@users.noreply.github.com>
2024-11-28 13:30:05 -0300

    Enviando atividade 03 - CRUD

--- src/main/java/com/blogpessoal/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/blogpessoal/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,20 @@
 package com.blogpessoal.blogpessoal.repository;
 
+import java.util.List; 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.blogpessoal.blogpessoal.model.Postagem;
 
+/* public: nível de acesso da interface
+* interface: é um contrato que define métodos que devem ser implementados por uma classe. No contexto do Spring Data JPA, interfaces como essa são usadas para definir repositórios (repositories) sem precisar implementar manualmente os métodos básicos, pois o framework faz isso automaticamente.
+* JpaRepository: É uma interface fornecida pelo Spring Data JPA. Ela já contém métodos prontos para operações no banco de dados, como save(), findById(), delete(), entre outros.
+* Postagem: É a entidade que será manipulada por esse repositório.
+* Long: Indica o tipo do ID da entidade Postagem. Nesse caso, o ID da tabela no banco de dados é do tipo Long.
+*/
+
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
